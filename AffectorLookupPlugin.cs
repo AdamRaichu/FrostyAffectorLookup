52537c689299e0d93f7f7a21b2f9597b4dad17a0
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2025-04-23 20:12:24 -0400

    first commit

---
+++ AffectorLookupPlugin.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using AffectorLookupPlugin.Windows;
+using Frosty.Controls;
+using Frosty.Core;
+using Frosty.Core.Controls;
+using Frosty.Core.Windows;
+using FrostySdk;
+using FrostySdk.IO;
+using FrostySdk.Managers;
+using FrostySdk.Resources;
+
+namespace AffectorLookupPlugin
+{
+    public class AffectorGenerateCacheMenuExtension : MenuExtension
+    {
+        public override string TopLevelMenuName => "Tools";
+        public override string SubLevelMenuName => "Generate Cache";
+        public override string MenuItemName => "Affector Lookup Cache";
+        public override ImageSource Icon => new ImageSourceConverter().ConvertFromString("pack://application:,,,/FrostyEditor;component/Images/Database.png") as ImageSource;
+
+        public static string CacheDirectory = System.AppDomain.CurrentDomain.BaseDirectory + @"Plugins\Caches\";
+        public static string CacheFilePath = CacheDirectory + Enum.GetName(typeof(ProfileVersion), ProfilesLibrary.DataVersion) + "AffectorLookupPlugin_Cache.cache";
+        public static int version = 0x00000002;
+
+        public override RelayCommand MenuItemClicked => new RelayCommand((o) =>
+        {
+            FrostyTaskWindow.Show("Creating Cache", "Enumerating EBX Files...", (task) =>
+            {
+                CreateCache(task);
+            });
+        });
+
+        private void CreateCache(FrostyTaskWindow task)
+        {
+            AssetManager AM = App.AssetManager;
+            List<EbxAssetEntry> ebxFiles = AM.EnumerateEbx(type: "AffectorAsset").ToList();
+            int index = 0;
+            int count = ebxFiles.Count;
+            List<UInt32> ids = new List<UInt32>();
+            List<string> reasons = new List<string>();
+            List<string> paths = new List<string>();
+            List<UInt32> idsWithDuplicates = new List<UInt32>();
+            ebxFiles.ForEach((affectorEntry) =>
+            {
+                if (affectorEntry.IsAdded) {
+                    App.Logger.Log("Skipping " + affectorEntry.Name + " as it is an added asset.");
+                    return;
+                }
+
+                EbxAsset affectorAsset = AM.GetEbx(affectorEntry);
+
+                task.Update($"Processing ({index + 1}/{count}): {affectorEntry.Filename}", index / count * 50);
+                dynamic root = affectorAsset.RootObject;
+                if (ids.Contains(root.Identifier)) { }
+                ids.Add(root.Identifier);
+                reasons.Add("asset.Identifier");
+                paths.Add(affectorEntry.Name);
+                if (ids.Contains(root.Identifier) && !idsWithDuplicates.Contains(root.Identifier))
+                {
+                    idsWithDuplicates.Add(root.Identifier);
+                }
+                List<UInt32> descriptorIds = (List<UInt32>) root.DescriptorIds;
+                for (int i = 0; i < descriptorIds.Count; i++) {
+                    ids.Add(descriptorIds[i]);
+                    reasons.Add($"asset.DescriptorIds[{i}]");
+                    paths.Add(affectorEntry.Name);
+                    if (ids.Contains(descriptorIds[i]) && !idsWithDuplicates.Contains(descriptorIds[i]))
+                    {
+                        idsWithDuplicates.Add(descriptorIds[i]);
+                    }
+                }
+                index++;
+            });
+            using (NativeWriter writer = new NativeWriter(new FileStream(CacheFilePath, FileMode.Create)))
+            {
+                // format version
+                writer.Write(version);
+                // number of ids
+                writer.Write(ids.Count);
+                // for each id, track...
+                for (int i = 0; i < ids.Count; i++)
+                {
+                    // the id itself
+                    writer.Write(ids[i]);
+                    // where it was referenced in the asset
+                    writer.WriteNullTerminatedString(reasons[i]);
+                    // the path to the asset
+                    writer.WriteNullTerminatedString(paths[i]);
+                }
+                // number of ids with duplicates
+                writer.Write(idsWithDuplicates.Count);
+                // for each id with duplicates, write the actual id
+                for (int i = 0; i < idsWithDuplicates.Count; i++)
+                {
+                    writer.Write(idsWithDuplicates[i]);
+                }
+            }
+        }
+    }
+
+    public class ViewAffectorLookupWindow : MenuExtension
+    {
+        // public override ImageSource Icon => new ImageSourceConverter().ConvertFromString("pack://application:,,,/AffectorLookupPlugin;component/Images/Hexagon.png") as ImageSource;
+
+        public override string TopLevelMenuName => "View";
+        public override string MenuItemName => "Affector Lookup";
+
+        public override RelayCommand MenuItemClicked => new RelayCommand(o =>
+        {
+            AffectorLookupWindow window = new AffectorLookupWindow();
+            if (!window.cacheReadFailed)
+                window.Show();
+        });
+    }
+}
---
+++ Windows/AffectorLookup.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Frosty.Controls;
+using Frosty.Core;
+using Frosty.Core.Windows;
+using FrostySdk;
+using FrostySdk.IO;
+
+namespace AffectorLookupPlugin.Windows
+{
+    public partial class AffectorLookupWindow : FrostyDockableWindow
+    {
+        private readonly Dictionary<uint, string> idToNameMap = new Dictionary<uint, string>();
+        private readonly Dictionary<uint, string> idToReasonMap = new Dictionary<uint, string>();
+        private readonly Dictionary<uint, bool> idToIsDupeMap = new Dictionary<uint, bool>();
+        public bool cacheReadFailed = false;
+        private bool lookupFailed = true;
+
+        public AffectorLookupWindow()
+        {
+            FrostyTaskWindow.Show("Loading Affector Cache", "Loading Affector Cache...", (task) =>
+            {
+                task.Update("Loading Cache", 0);
+                if (LoadCache(task))
+                {
+                    task.Update("Cache Loaded", 100);
+                }
+                else
+                {
+                    task.Update("Cache Load Failed", 100);
+                    cacheReadFailed = true;
+                }
+            });
+            InitializeComponent();
+            if (cacheReadFailed)
+            {
+                Close();
+            }
+        }
+
+        private void IdInput_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            uint value;
+            try
+            {
+                value = Convert.ToUInt32(IdInput.Text, CultureInfo.InvariantCulture);
+            }
+            catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+            {
+                AffectorPath.Text = ex is FormatException ? "Invalid Id (make sure you only put in numbers)" : "Invalid Id (value too large to be a UInt32)";
+                AffectorReason.Text = "Reference details will appear here";
+                lookupFailed = true;
+                setOpenAssetButton_IsEnabled(lookupFailed);
+                return;
+            }
+
+            bool foundValue = idToNameMap.TryGetValue(value, out string name);
+            idToReasonMap.TryGetValue(value, out string reason);
+            idToIsDupeMap.TryGetValue(value, out bool isDupe);
+            isDupe = isDupe || false;
+
+            if (foundValue)
+            {
+                AffectorPath.Text = name;
+                AffectorReason.Text = "Id referenced is " + reason;
+                DuplicateWarning.Visibility = isDupe ? Visibility.Visible : Visibility.Collapsed;
+                lookupFailed = false;
+            }
+            else
+            {
+                AffectorPath.Text = "Asset not found for id. (Maybe this id is from an added affector?)";
+                lookupFailed = true;
+            }
+            setOpenAssetButton_IsEnabled(lookupFailed);
+        }
+
+        private void setOpenAssetButton_IsEnabled(bool failure)
+        {
+            OpenAssetButton.IsEnabled = !failure;
+        }
+
+        private void OpenAssetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (lookupFailed) return;
+
+            Close();
+
+            App.EditorWindow.OpenAsset(App.AssetManager.GetEbxEntry(AffectorPath.Text));
+        }
+
+        private void OkButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private bool LoadCache(FrostyTaskWindow task)
+        {
+            string cacheFilePath = AffectorGenerateCacheMenuExtension.CacheFilePath;
+            if (!File.Exists(cacheFilePath))
+            {
+                FrostyMessageBox.Show("Cache file not found. Please generate the cache first through \"Tools>Generate Cache>Affector Cache\".");
+                return false;
+            }
+            using (NativeReader reader = new NativeReader(new FileStream(cacheFilePath, FileMode.Open)))
+            {
+                int version = reader.ReadInt();
+                if (version != AffectorGenerateCacheMenuExtension.version)
+                {
+                    FrostyMessageBox.Show($"Cache file version mismatch. Please generate the cache again. (file: {version}, expected: {AffectorGenerateCacheMenuExtension.version})");
+                    return false;
+                }
+                int count = reader.ReadInt();
+                for (int i = 0; i < count; i++)
+                {
+                    uint id = reader.ReadUInt();
+                    string reason = reader.ReadNullTerminatedString();
+                    string name = reader.ReadNullTerminatedString();
+                    idToReasonMap[id] = reason;
+                    idToNameMap[id] = name;
+                }
+                int dupeCount = reader.ReadInt();
+                for (int i = 0; i < dupeCount; i++)
+                {
+                    uint id = reader.ReadUInt();
+                    idToIsDupeMap[id] = true;
+                }
+            }
+            return true;
+        }
+    }
+}
